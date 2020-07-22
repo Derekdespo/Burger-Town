0abadf7f8e6e24f8826d09a813e5f1ea52d719f4
Derek Desposito <derekdespo@icloud.com>
2020-07-21 21:51:47 -0400

    working buttons

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,22 @@
-// Set up a MYSQL connection
-// Set a variable that will serve for requiring the mysql npm package
+// Set up MySQL connection.
 var mysql = require("mysql");
 
-// Typical set up to run a MYSQL connection from the npm's docs
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-  });
-  
-  // Make connection.
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-    console.log("connected as id " + connection.threadId);
-  });
-  
-  // Export connection for our ORM to use.
-  module.exports = connection;
-  
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Jisoo1017!",
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
--- db/schema.sql
+++ db/schema.sql
@@ -3,10 +3,10 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers_db
+CREATE TABLE burgers
 (
     id INT NOT NULL AUTO_INCREMENT,
-    burger_name VARCHAR(255) NOT NULL,
-    eaten BOOLEAN DEFAULT falsse, 
+    name VARCHAR(255) NOT NULL,
+    eaten BOOLEAN DEFAULT false, 
     PRIMARY KEY (id)
 )
